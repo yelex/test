1819dab1e197a9f08100294fe7bb534581efd1d8
Your Name <vtb@example.com>
2024-01-04 01:32:54 +0300

    feat: add soup text

--- myfile.py
+++ myfile.py
@@ -5,7 +5,7 @@ import time
 
 
 options = uc.ChromeOptions()
-options.add_argument("--headless");
+# options.add_argument("--headless");
 options.add_argument("--no-sandbox");
 options.add_argument("--disable-dev-shm-usage");
 options.add_argument("--disable-browser-side-navigation");
