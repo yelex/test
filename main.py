c0e1ad582ea7dec5d51faea05ee36903296b5f5e
Your Name <vtb@example.com>
2024-01-04 21:42:48 +0300

    fix: some fix in main.py

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+# from get_fake_headers import get_headers
 
+HEADERS = {'Accept': '*/*', 
+           'Connection': 'keep-alive',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36', 
+           'Accept-Encoding': 'gzip, deflate, br', 
+           'Accept-Language': 'en-US;q=0.5,en;q=0.3', 'Cache-Control': 'max-age=0', 
+           'DNT': '1', 'Upgrade-Insecure-Requests': '1', 'Referer': 'https://google.com'}
 
-HEADERS = {'Accept': '*/*', 'Connection': 'keep-alive', 
-           'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.109 Safari/537.36', 
-           'Referer': 'https://google.com', 'Pragma': 'no-cache'}
+# HEADERS = get_headers()
 
 # # Above should print an IP different than your public IP
 
@@ -41,8 +46,9 @@ urls = pd.read_csv(r'./final_links.csv', index_col=0, sep=';')
 urls_vprok = urls.loc[urls['site_code']=='perekrestok','site_link']
 
 for link in urls_vprok:
+    time.sleep(10)
     print(link)
-    time.sleep(5)
+    
     classes = dict()
     classes['title'] = ['h1', 'Title_title__nvodu']
     classes['price_regular'] = ['span', 'Price_price__QzA8L Price_size_XL__MHvC1 Price_role_regular__X6X4D']
