2e30097a955bfde9021ec1a1da95285f7530fd54
Alexey Evseev <evseev_alexey94@bk.ru>
2024-01-17 23:27:09 +0300

    feat: optomize renew_tor_ip

--- utils/update_ip.py
+++ utils/update_ip.py
@@ -1,13 +1,17 @@
 from stem import Signal
 from stem.control import Controller
 import time
+from platform import platform
+from ip import get_ip_info
 
-def renew_tor_ip(password='password'):
+def renew_tor_ip():
+    password = 'mypassword' if 'mac' in platform() else 'password'
     with Controller.from_port(port = 9051) as controller:
         controller.authenticate(password=password)
-        controller.signal(Signal.NEWNYM)        
+        controller.signal(Signal.NEWNYM)      
         
 if __name__ == "__main__":
     for i in range(5):
         renew_tor_ip()
+        get_ip_info()
         time.sleep(5)
