aaa7870bc2a212cc641a81d035309483adb39d13
junibox <win@junibox.com>
2024-01-17 23:51:49 +0300

    feat: add print ip

--- utils/update_tor_ip.py
+++ utils/update_tor_ip.py
@@ -1,6 +1,9 @@
 import time
 from platform import platform
-from utils.print_ip import get_ip_info
+from print_ip import print_ip
+from stem.control import Controller
+from stem.control import Signal
+
 
 def update_tor_ip():
     password = 'mypassword' if 'mac' in platform() else 'password'
@@ -11,5 +14,5 @@ def update_tor_ip():
 if __name__ == '__main__':
     for i in range(5):
         update_tor_ip()
-        get_ip_info()
+        print_ip(is_tor=True)
         time.sleep(5)
