1cb431858a4d79dfc385c50f2f27e83b366b6795
junibox <win@junibox.com>
2024-01-28 02:53:08 +0300

    refactor: add pylint fix

--- utils/ip.py
+++ utils/ip.py
@@ -1,21 +1,20 @@
-from utils.global_state import Global
 import json
 import time
 from platform import platform
 from stem.control import Controller
-from stem.control import Signal
+from utils.global_state import Global
 
-def print_ip(is_tor=True):
+def print_ip(is_tor: bool =True) -> None:
     session = Global().tor_session if is_tor else Global().request_session
     json_ip = session.get('https://httpbin.org/ip').text
     ip = json.loads(json_ip)['origin']
     print(f"{'Tor IP' if is_tor else 'IP'}: {ip}")
 
-def update_tor_ip():
+def update_tor_ip() -> None:
     password = 'mypassword' if 'mac' in platform() else 'password'
     with Controller.from_port(port = 9051) as controller:
         controller.authenticate(password=password)
-        controller.signal(Signal.NEWNYM)  
+        controller.signal("NEWNYM")
 
 if __name__ == '__main__':
     for i in range(5):
