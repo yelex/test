46fb3608a6129a5520b244542cf47816245fa407
Alexey Evseev <evseev_alexey94@bk.ru>
2024-05-01 13:02:45 +0300

    fix: changes in globalse

--- scrappers/globus.py
+++ scrappers/globus.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import sys
 import os
 import re
-
-sys.path.insert(0, os.path.abspath('./'))
+import pprint
+sys.path.insert(0, os.path.abspath("./"))
 
 from utils.tools import wspex_space
 from utils.constants import HEADERS_GLOBUS, TIMEOUT
@@ -11,96 +11,107 @@ from globals.global_state import Global
 
 classes = dict()
 
-classes['products_div'] = ['div', 'item-card__content--right']
-classes['price_div'] = ['span', 'item-price']
+classes["products_div"] = ["div", "css-1hj20p7 css-146vzrl"]
 
-
-classes['title'] = ['h1', 'js-with-nbsp-after-digit']
-classes['price_regular_rub'] = ['span', 'item-price__rub']
-classes['price_regular_kop'] = ['span', 'item-price__kop']
-classes['price_old'] = ['span', 'item-price__old']
-classes['site_unit'] = ['span', 'item-price__unit']
-classes['is_avaliable'] = ['div', 'h2 item-available-title']
+classes["title"] = ["h1", "css-qs82cc"]
+classes["price_regular_rub"] = ["div", "css-10fcdyq"]
+classes["price_regular_kop"] = ["div", "css-1vx8352"]
+classes["price_old_rub"] = ["div", "css-19o902r"]
+classes["price_old_kop"] = ["div", "css-1mh8qie"]
+classes["site_unit"] = ["div", "css-1im3s12"]
+classes["is_avaliable"] = ["div", "h2 item-available-title"]
 
 
 global_ = Global()
 
-def get_data_from_link(link, global_=global_,
-                       headers=HEADERS_GLOBUS, timeout=TIMEOUT):
-    '''
+
+def get_data_from_link(link, global_=global_, headers=HEADERS_GLOBUS, timeout=TIMEOUT):
+    """
     Сделать сессию вовне
     Если ошибка - возвращать False, инициировать сессию с тор и возвращать сюда же
-    '''
+    """
 
     if global_.is_tor_globus:
         print(global_.tor_session)
         r = global_.tor_session.get(link, headers=headers, timeout=timeout)
     else:
-        print(link + '\n')
+        print(link + "\n")
         r = global_.request_session.get(link, headers=headers, timeout=timeout)
 
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    products_div = soup.find(classes['products_div'][0], {'class': classes['products_div'][-1]})
+    soup = BeautifulSoup(r.content, "html.parser")
+    print(r.content)
+    print(soup)
+    products_div = soup.find(
+        classes["products_div"][0], {"class": classes["products_div"][-1]}
+    )
 
     if not products_div:
-        print('  Нет товара по ссылке\n')
+        print("  Нет товара по ссылке\n")
         return False
-    
-    title_div = products_div.find(classes['title'][0], {'class': classes['title'][-1]})
+
+    title_div = products_div.find(classes["title"][0], {"class": classes["title"][-1]})
     if not title_div:
-        print('  Нет названия\n')
+        print("  Нет названия\n")
         return False
- 
-    is_avaliable = products_div.find(classes['is_avaliable'][0], {'class': classes['is_avaliable'][-1]})['style']
-    if 'none' not in is_avaliable:
-        print(' Товар временно отсутствует!')
-        return False
-    
+
+    # is_avaliable = products_div.find(
+    #     classes["is_avaliable"][0], {"class": classes["is_avaliable"][-1]}
+    # )["style"]
+
+    # if "none" not in is_avaliable:
+    #     print(" Товар временно отсутствует!")
+    #     return False
+
     title = wspex_space(title_div.text)
 
-    price_div = products_div.find(classes['price_div'][0], {'class': classes['price_div'][-1]})
+    price_text_rub_div = products_div.find(
+        classes["price_regular_rub"][0], {"class": classes["price_regular_rub"][-1]}
+    )
+    price_text_kop_div = products_div.find(
+        classes["price_regular_kop"][0], {"class": classes["price_regular_kop"][-1]}
+    )
+    price_text_old_rub_div = products_div.find(
+        classes["price_old_rub"][0], {"class": classes["price_old_rub"][-1]}
+    )
 
-    price_text_rub_div = price_div.find(classes['price_regular_rub'][0], {'class': classes['price_regular_rub'][-1]})
-    price_text_kop_div = price_div.find(classes['price_regular_kop'][0], {'class': classes['price_regular_kop'][-1]})
-    price_text_old_div = price_div.find(classes['price_old'][0], {'class': classes['price_old'][-1]})
+    price_text_old_kop_div = products_div.find(
+        classes["price_old_kop"][0], {"class": classes["price_old_kop"][-1]}
+    )
 
     if not price_text_rub_div or not price_text_kop_div:
         return False
 
     try:
-        price_new = int(price_text_rub_div.text.replace(" ", "")) + \
-                                    0.01 * int(price_text_kop_div.text)
+        price_new = int(price_text_rub_div.text.replace(" ", "")) + 0.01 * int(
+            price_text_kop_div.text
+        )
     except:
-        price_new = int(price_text_rub_div.text.replace("\xa0", "")) + \
-                                    0.01 * int(price_text_kop_div.text)
+        price_new = int(price_text_rub_div.text.replace("\xa0", "")) + 0.01 * int(
+            price_text_kop_div.text
+        )
 
-    if price_text_old_div:
-        list_ = re.findall(r'\s+', wspex_space(price_text_old_div.text))
+    if price_text_old_rub_div:
+        price_old_rub = price_text_old_rub_div.text
+        price_old_kop = price_text_old_kop_div.text
 
-        if len(list_) == 2:
-            price_text = re.sub(r'\s+', '', wspex_space(price_text_old_div.text), count=1)
-            price_text = re.sub(r'\s+', '.', price_text)
-
-        else:
-            price_text = re.sub(r'\s+', '.', wspex_space(price_text_old_div.text))
-
-        price_old = float(price_text)
+        price_old = int(price_old_rub + '.' + price_old_kop)
 
     else:
         price_old = -1.0
-    site_unit = products_div.find(classes['site_unit'][0], 
-                                  {'class': classes['site_unit'][-1]}).text.strip()
+    site_unit = products_div.find(
+        classes["site_unit"][0], {"class": classes["site_unit"][-1]}
+    ).text.strip()
 
-    return {'site_title': title, 
-            'price_new': price_new, 
-            'price_old': price_old, 
-            'site_unit': site_unit}
+    return {
+        "site_title": title,
+        "price_new": price_new,
+        "price_old": price_old,
+        "site_unit": site_unit,
+    }
 
-if __name__ == '__main__':
-    link = 'https://online.globus.ru/products/muka-pshenichnaya-globus-khlebopekarnaya-2-kg/'
-    link = 'https://online.globus.ru/products/svinoy-okorok-svyshe-5-kg-1-upakovka-5-6-kg/'
+
+if __name__ == "__main__":
+    # link = 'https://online.globus.ru/products/muka-pshenichnaya-globus-khlebopekarnaya-2-kg/'
+    # link = 'https://online.globus.ru/products/svinoy-okorok-svyshe-5-kg-1-upakovka-5-6-kg/'
+    link = "https://online.globus.ru/products/kotlety-kurinye-petruxa-master-domashnie-s-gribami-i-syrom-450-g-729995_ST"
     print(get_data_from_link(link))
-
-
-    
--- utils/constants.py
+++ utils/constants.py
@@ -16,7 +16,20 @@ HEADERS_VPROK = {
 }
 
 HEADERS_GLOBUS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15"
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
+    'Cookie': 'i=AelZhzQvtGw2ryGDaN7JGaz9O02WNtdATOdsKjnnfEGg9R2s7a9NQXNLaN2zclo6EB5+Q/RkPMq+67rWiduW7v/Kwn4=; yandexuid=8190459421711055783; yashr=4313643991711055783; yuidss=8190459421711055783; ymex=2026539258.yrts.1711179258; _ym_uid=1711179263257287820; yandex_gid=213; _ym_d=1711179822; gdpr=0; yandex_login=e-alexey94; Session_id=3:1711796618.5.0.1711181348164:1gryLg:89.1.2:1|734956111.0.2.3:1711181348|3:10285281.590607.khbRxrJkto7PTFeHyFYofZ188B4; sessar=1.1188.CiA4Yke5mIE69BEETs0xudXLfff55akdrHYZXeF1Hw_4Ag.uTfunzh6-fOVWTKyrDSjfbwR9Z-VRCno1WzzM2R-ukw; sessionid2=3:1711796618.5.0.1711181348164:1gryLg:89.1.2:1|734956111.0.2.3:1711181348|3:10285281.590607.fakesign0000000000000000000; bh=EkEiTWljcm9zb2Z0IEVkZ2UiO3Y9IjEyMyIsICJOb3Q6QS1CcmFuZCI7dj0iOCIsICJDaHJvbWl1bSI7dj0iMTIzIhoFIng4NiIiDyIxMjMuMC4yNDIwLjY1IioCPzA6CSJXaW5kb3dzIkIIIjE1LjAuMCJKBCI2NCJSWiJNaWNyb3NvZnQgRWRnZSI7dj0iMTIzLjAuMjQyMC42NSIsIk5vdDpBLUJyYW5kIjt2PSI4LjAuMC4wIiwiQ2hyb21pdW0iO3Y9IjEyMy4wLjYzMTIuODciIg==; is_gdpr=0; is_gdpr_b=CLj5IhDA8wEoAg==; bh=Ej8iTWljcm9zb2Z0IEVkZ2UiO3Y9IjEyMyIsIk5vdDpBLUJyYW5kIjt2PSI4IiwiQ2hyb21pdW0iO3Y9IjEyMyIaBSJ4ODYiIg8iMTIzLjAuMjQyMC42NSIqAj8wOgkiV2luZG93cyJCCCIxNS4wLjAiSgQiNjQiUloiTWljcm9zb2Z0IEVkZ2UiO3Y9IjEyMy4wLjI0MjAuNjUiLCJOb3Q6QS1CcmFuZCI7dj0iOC4wLjAuMCIsIkNocm9taXVtIjt2PSIxMjMuMC42MzEyLjg3IiI=; yp=2027163988.pcs.1#4294967295.skin.s#1713771815.ygu.1#2026541348.udn.cDplLWFsZXhleTk0#1712401917.szm.1%3A2560x1440%3A1270x1308; yabs-sid=1797179211712476995',
+    'Origin': 'https://online.globus.ru',
+    'Referer': 'https://online.globus.ru/',
+    'Sec-Ch-Ua': '"Microsoft Edge";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0'
+
 }
 
 TIMEOUT = 120
