d1ec92a9e5d160930686cc6ea4343dc9bef65898
Алексей Евсеев <evseev_alexey94@bk.ru>
2024-06-05 00:31:31 +0300

    sdfsd

--- scrappers/globus.py
+++ scrappers/globus.py
@@ -35,7 +35,7 @@ def get_data_from_link(link, global_=global_, headers=HEADERS_GLOBUS, timeout=TI
     print(12312)
     driver.get(link)
     print(link)
-    time.sleep(50)
+    time.sleep(5)
     soup = BeautifulSoup(driver.page_source, 'lxml')
 
 
