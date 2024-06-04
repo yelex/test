72a522eeb2694a35ecfbe8c6c9972382d9c69739
Алексей Евсеев <evseev_alexey94@bk.ru>
2024-06-05 00:51:26 +0300

    asdasd

--- globals/global_state.py
+++ globals/global_state.py
@@ -43,10 +43,13 @@ class Global(Singleton):
               'Chrome/123.0.0.0 Safari/537.36'
 
         options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("--start-maximized")
+
+        options.add_argument("--headless");
+        options.add_argument("--window-size=1920,1080");
+        options.add_argument("--no-sandbox");
+        options.add_argument("--disable-extensions");
+        options.add_argument("--dns-prefetch-disable");
+        options.add_argument("--disable-gpu");
         options.add_argument(f'user-agent={user_agent}')
         options.add_experimental_option(
         "prefs", {
