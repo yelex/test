abd73f2cc816cd6283a1ecac37b568a2452d7fb4
Алексей Евсеев <evseev_alexey94@bk.ru>
2024-06-07 19:53:57 +0300

    cad

--- scrappers/globus.py
+++ scrappers/globus.py
@@ -36,7 +36,7 @@ def get_data_from_link(link, global_=global_, headers=HEADERS_GLOBUS, timeout=TI
     # driver = global_.webdriver
     service = Service(executable_path='./chromedrivers/chromedriver')
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    options.add_argument('--headless=new')
     # options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=service, options=options)
